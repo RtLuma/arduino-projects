176cc569ebf2fc907e68d3fd18ac436e112164bf
Michael Bartlett <bartlettmic@gmail.com>
2018-06-22 23:46:29 -0600

    AND IT'S GOOOOD

--- RoomLEDs/Uno_Slave_Reader.ino/Visualizer.h
+++ RoomLEDs/Uno_Slave_Reader.ino/Visualizer.h
@@ -1,10 +1,26 @@
-#define SECTION 23
+#define SECTION 21
 
 extern const uint8_t hues[BANDS];
 
 uint8_t sclera[2][BANDS + 1][3] = {0};
 uint8_t iris[3] = { 0 };
 uint8_t pupil[3] = { 0 };
+
+#define sendSclera pixelAr = pixelA[0];\
+  pixelAg = pixelA[1];\
+  pixelAb = pixelA[2];\
+  pixelBr = pixelB[0];\
+  pixelBg = pixelB[1];\
+  pixelBb = pixelB[2];\
+  dist = SECTION + (band%2);\
+  for (uint8_t p = 0; p < SECTION + (band%2); p++) {\
+    dist--;\
+    sendPixel(\
+              ((pixelAr * dist) + (pixelBr * p)) / SECTION,\
+              ((pixelAg * dist) + (pixelBg * p)) / SECTION,\
+              ((pixelAb * dist) + (pixelBb * p)) / SECTION\
+             );\
+  }
 
 
 void Chroma(void) {
@@ -31,64 +47,31 @@ void Chroma(void) {
   }
 
   /////////////////////////////// start computing and broadcasting each pixel on the fly
-
-  int8_t nextband;
-  uint8_t dist, band;
+  uint8_t band, dist;
   uint8_t pixelAr, pixelAg, pixelAb, pixelBr, pixelBg, pixelBb;
   uint8_t *pixelA, *pixelB;
 
 
-  for (band = 0; band < BANDS; band++) {
-
-    dist = SECTION;
-
-    pixelA = sclera[0][band];
-    pixelB = sclera[0][band + 1];
-
-    pixelAr = pixelA[0];
-    pixelAg = pixelA[1];
-    pixelAb = pixelA[2];
-
-    pixelBr = pixelB[0];
-    pixelBg = pixelB[1];
-    pixelBb = pixelB[2];
-
-    for (uint8_t p = 0; p < SECTION; p++) {
-      dist--;
-      sendPixel(
-        ((pixelAr * dist) + (pixelBr * p)) / SECTION,
-        ((pixelAg * dist) + (pixelBg * p)) / SECTION,
-        ((pixelAb * dist) + (pixelBb * p)) / SECTION
-      );
-    }
+  for (band=3; band < BANDS - 1; band++) {
+    pixelA = sclera[0][band]; pixelB = sclera[0][band + 1];
+    sendSclera
   }
+
+  pixelA = sclera[0][BANDS - 1]; pixelB = sclera[1][BANDS - 1];
+  sendSclera
 
   for (band = BANDS - 1; band > 0; band--) {
-    dist = SECTION;
-
-    pixelA = sclera[1][band];
-    pixelB = sclera[1][band - 1];
-
-    pixelAr = pixelA[0];
-    pixelAg = pixelA[1];
-    pixelAb = pixelA[2];
-
-    pixelBr = pixelB[0];
-    pixelBg = pixelB[1];
-    pixelBb = pixelB[2];
-
-    for (uint8_t p = 0; p < SECTION; p++) {
-      dist--;
-      sendPixel(
-        ((pixelAr * dist) + (pixelBr * p)) / SECTION,
-        ((pixelAg * dist) + (pixelBg * p)) / SECTION,
-        ((pixelAb * dist) + (pixelBb * p)) / SECTION
-      );
-    }
+    pixelA = sclera[1][band]; pixelB = sclera[1][band - 1];
+    sendSclera
   }
-  
-  
-  
+
+  pixelA = sclera[1][0];pixelB = sclera[0][0];
+  sendSclera
+
+  for (band=0; band < 3; band++) {
+    pixelA = sclera[0][band]; pixelB = sclera[0][band + 1];
+    sendSclera
+  }
   //sendPixel(sclera[1][0][0], sclera[1][0][1], sclera[1][0][2]);
 
 
@@ -109,14 +92,18 @@ void Chroma(void) {
     newPupil[2] += (sclera[2][band][2] + sclera[1][band][2]) >> 1;
   }
 
-  iris[0] = ((newIris[0] >> 2) + iris[0]) >> 1;
-  iris[1] = ((newIris[1] >> 2) + iris[1]) >> 1;
-  iris[2] = ((newIris[2] >> 2) + iris[2]) >> 1;
+  newIris[0] >>= 2; newIris[1] >>= 2; newIris[2] >>= 2; 
+  
+  iris[0] = (newIris[0] + newIris[0] + newIris[0] + iris[0]) >> 2;
+  iris[1] = (newIris[1] + newIris[1] + newIris[1] + iris[1]) >> 2;
+  iris[2] = (newIris[2] + newIris[2] + newIris[2] + iris[2]) >> 2;
 
-  pupil[0] = ((newPupil[0] >> 2) + pupil[0]) >> 1;
-  pupil[1] = ((newPupil[1] >> 2) + pupil[1]) >> 1;
-  pupil[2] = ((newPupil[2] >> 2) + pupil[2]) >> 1;
-
+  newPupil[0] >>= 2; newPupil[1] >>= 2; newPupil[2] >>= 2; 
+  
+  pupil[0] = (newPupil[0] + newPupil[0] + newPupil[0] + pupil[0]) >> 2;
+  pupil[1] = (newPupil[1] + newPupil[1] + newPupil[1] + pupil[1]) >> 2;
+  pupil[2] = (newPupil[2] + newPupil[2] + newPupil[2] + pupil[2]) >> 2;
+  
   for (p; p < 200; p++) sendPixel2(iris[0], iris[1], iris[2]);
   for (p; p < 300; p++) sendPixel2(pupil[0], pupil[1], pupil[2]);
 }
