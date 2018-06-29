5ad12dad9628f108075a22f1bd28af4c5812aa83
Michael Bartlett <bartlettmic@gmail.com>
2018-06-29 08:22:34 -0600

    now gotta fix user-color protocol

--- RoomLEDs/ESP8266_Master_Writer/ESP8266_Master_Writer.ino
+++ RoomLEDs/ESP8266_Master_Writer/ESP8266_Master_Writer.ino
@@ -32,86 +32,87 @@ void setup() {
       return server.requestAuthentication();
     String message = "";
     bool succesfulQuery = true;
+    
+        if (server.args()) {
+          byte length = server.args() << 1;
+    
+          char payload[length];
+    
+          for (uint8_t i = 0; i < length; i += 2) {
+            payload[i] = server.argName(i)[0];
+    
+            if (payload[i]=='m') payload[i + 1] = server.arg(i)[0];
+            else payload[i + 1] = server.arg(i).toInt();
+          }
+    
+          succesfulQuery = sendBytes(payload, length);
+    
+          for (uint8_t i = 0; i < server.args(); i++) {
+            char jews[] = { payload[i], '\0' };
+            message += String(jews);
+            message += ": ";
+            message += server.arg(i);
+            message += " ";
+          }
+        }
+        else message = "No args";
 
-    if (server.args()) {
-      byte length = server.args() << 1;
-
-      char payload[length];
-
-      for (uint8_t i = 0; i < length; i += 2) {
-        payload[i] = server.argName(i)[0];
-
-        if (payload[i]=='m') payload[i + 1] = server.arg(i)[0];
-        else payload[i + 1] = server.arg(i).toInt();
-      }
-
-      succesfulQuery = sendBytes(payload, length);
-
-      for (uint8_t i = 0; i < length; i += 2) {
-        message += payload[i];
-        message += ": ";
-        message += server.arg(i);
-        message += " ";
-      }
-    }
-    else message = "No args";
-
-    if (succesfulQuery) server.send(200, "text/plain", "dicks");
+    if (succesfulQuery) server.send(200, "text/plain", message);
     else server.send(404, "text/plain", "I2C error?");
   });
-  
+
   ////////////////////////////////////////////////////////////////////
 
 
-//  server.on("/m", []() {
-//    if (!server.authenticate(www_username, www_password))
-//      return server.requestAuthentication();
-//
-//    if (!server.args()) {             // .../m?f HOPEFULLY!
-//      server.send(400, "text/plain", "Must specify mode label");
-//      return;
-//    }
-//
-//    char payload[2];
-//    payload[0] = 'm';
-//    payload[1] = server.argName(0)[0];
-//
-//    if (sendBytes(payload, 2)) {
-//      String msg = "Mode changed? Got label ";
-//      msg += payload[1];
-//      server.send(200, "text/plain", msg);
-//    }
-//    else server.send(404, "text/plain", "I2C error");
-//  });
+  //  server.on("/m", []() {
+  //    if (!server.authenticate(www_username, www_password))
+  //      return server.requestAuthentication();
+  //
+  //    if (!server.args()) {             // .../m?f HOPEFULLY!
+  //      server.send(400, "text/plain", "Must specify mode label");
+  //      return;
+  //    }
+  //
+  //    char payload[2];
+  //    payload[0] = 'm';
+  //    payload[1] = server.argName(0)[0];
+  //
+  //    if (sendBytes(payload, 2)) {
+  //      String msg = "Mode changed? Got label ";
+  //      msg += payload[1];
+  //      server.send(200, "text/plain", msg);
+  //    }
+  //    else server.send(404, "text/plain", "I2C error");
+  //  });
 
-//  server.on("/c", []() {  // Change global color
-//    if (!server.authenticate(www_username, www_password))
-//      return server.requestAuthentication();
-//
-//    /*
-//      String arg(String name);        // get request argument value by name
-//      String arg(int i);              // get request argument value by number
-//      String argName(int i);          // get request argument name by number
-//      int args();                     // get arguments count
-//      bool hasArg(String name);       // check if argument exists*/
-//
-//    if (!(server.hasArg("r") && server.hasArg("g") && server.hasArg("b"))) {
-//      server.send(400, "text/plain", "Malformed RGB format");
-//      return;
-//    }
-//
-//    char payload[] = {
-//      'c',//olor
-//      server.arg("r").toInt(),
-//      server.arg("g").toInt(),
-//      server.arg("b").toInt()
-//    };
-//
-//    if (sendBytes(payload, ARRAY_SIZE(payload)))
-//      server.send(200, "text/plain",
-//                  "r: " + server.arg("r") + ", g: " + server.arg("g") + ", b: " + server.arg("b"));
-//    else server.send(404, "text/plain", "I2C error");
-//  });
+  //  server.on("/c", []() {  // Change global color
+  //    if (!server.authenticate(www_username, www_password))
+  //      return server.requestAuthentication();
+  //
+  //    /*
+  //      String arg(String name);        // get request argument value by name
+  //      String arg(int i);              // get request argument value by number
+  //      String argName(int i);          // get request argument name by number
+  //      int args();                     // get arguments count
+  //      bool hasArg(String name);       // check if argument exists*/
+  //
+  //    if (!(server.hasArg("r") && server.hasArg("g") && server.hasArg("b"))) {
+  //      server.send(400, "text/plain", "Malformed RGB format");
+  //      return;
+  //    }
+  //
+  //    char payload[] = {
+  //      'c',//olor
+  //      server.arg("r").toInt(),
+  //      server.arg("g").toInt(),
+  //      server.arg("b").toInt()
+  //    };
+  //
+  //    if (sendBytes(payload, ARRAY_SIZE(payload)))
+  //      server.send(200, "text/plain",
+  //                  "r: " + server.arg("r") + ", g: " + server.arg("g") + ", b: " + server.arg("b"));
+  //    else server.send(404, "text/plain", "I2C error");
+  //  });
 
   server.onNotFound([]() {
 
@@ -127,6 +128,10 @@ void setup() {
       message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
     }
     server.send(404, "text/plain", message);
+  });
+
+  server.on("/inline", []() {
+    server.send(200, "text/plain", "this works as well");
   });
 
   server.begin();
