0e6975185695c5a6192c85f9657ce09f9cc54bb4
Michael Bartlett <bartlettmic@gmail.com>
2018-07-26 23:06:06 -0600

    linked

--- RoomLEDs/gradient.cpp
+++ RoomLEDs/gradient.cpp
@@ -3,7 +3,7 @@
 #include <unistd.h>
 using namespace std;
 
-#define SECTION 100
+#define SECTION 50
 #define BANDS 7
 
 uint8_t sclera[2][BANDS][3] = {0};
@@ -17,7 +17,7 @@ string val2block(uint8_t val) {
   if (val > 95)  return "\u2584";
   if (val > 63)  return "\u2583";
   if (val > 31)  return "\u2582";
-  return "_";
+  return " ";
 } 
 
 void printColor(uint8_t r, uint8_t g, uint8_t b) {
@@ -26,18 +26,19 @@ void printColor(uint8_t r, uint8_t g, uint8_t b) {
   printf("%c[%dm", 0x1B, 34); cout << val2block(b);
   printf("%c[%dm", 0x1B, 0);
   fflush(stdout);
-  usleep(10000);
-
+  usleep(20000);
 }
+//]]]]
 
 #define sendVisualizerPixels(DIST)\
-  r = pixelA[0]; g = pixelA[1]; b = pixelA[2];\
-  printf("%d,%d,%d -> %d,%d,%d over %d steps\n",r,g,b,pixelB[0],pixelB[1],pixelB[2],SECTION);\
-  cin.ignore();\
-  r_step = ((int16_t)(pixelB[0] - pixelA[0]) << 8)/DIST;\
-  g_step = ((int16_t)(pixelB[1] - pixelA[1]) << 8)/DIST;\
-  b_step = ((int16_t)(pixelB[2] - pixelA[2]) << 8)/DIST;\
-  for (uint8_t p = 0; p < DIST; p++) {\
+  r = pixelA[0] << 8;\
+  g = pixelA[1] << 8;\
+  b = pixelA[2] << 8;\
+  printf("%d,%d,%d -> %d,%d,%d over %d steps:\n",r>>8,g>>8,b>>8,pixelB[0],pixelB[1],pixelB[2],SECTION);\
+  r_step = (((uint16_t)pixelB[0] << 8) - ((uint16_t)pixelA[0] << 8))/DIST;\
+  g_step = (((uint16_t)pixelB[1] << 8) - ((uint16_t)pixelA[1] << 8))/DIST;\
+  b_step = (((uint16_t)pixelB[2] << 8) - ((uint16_t)pixelA[2] << 8))/DIST;\
+ for (uint8_t p = 0; p < DIST; p++) {\
     printColor(r>>8, g>>8, b>>8);\
     r += r_step; g += g_step; b += b_step;\
   }\
@@ -47,14 +48,13 @@ printf("\r");\
 
 int main() {
 
-  /////////////////////////////// start computing and broadcasting each pixel on the fly
   uint16_t r, g, b;
   int16_t r_step, g_step, b_step;
   uint8_t *pixelA, *pixelB;
   
-  sclera[0][1][0] = 255; sclera[0][1][1] = 255; sclera[0][1][2] = 255;
-  sclera[0][0][0] = 0; sclera[0][0][1] = 0; sclera[0][0][2] = 0;
-  sclera[1][0][0] = 0; sclera[1][0][1] = 255; sclera[1][0][2] = 64;
+  sclera[0][1][0] = 255; sclera[0][1][1] = 64; sclera[0][1][2] = 255;
+  sclera[0][0][0] = 0; sclera[0][0][1] = 255; sclera[0][0][2] = 0;
+  sclera[1][0][0] = 0; sclera[1][0][1] = 127; sclera[1][0][2] = 64;
   sclera[1][1][0] = 64; sclera[1][1][1] = 0; sclera[1][1][2] = 255;
 
   pixelA = sclera[0][0]; pixelB = sclera[0][1]; sendVisualizerPixels(SECTION)
--- RoomLEDs/test.cpp
+++ RoomLEDs/test.cpp
@@ -1,68 +1,130 @@
-#include <string>
-#include <iostream>
-#include <unistd.h>
+#include<iostream>
 using namespace std;
-
-#define SECTION 50
-#define BANDS 7
-
-uint8_t sclera[2][BANDS][3] = {0};
-
-string val2block(uint8_t val) {
-  // return to_string(val) + " ";
-  if (val > 223) return "\u2588";
-  if (val > 191) return "\u2587";
-  if (val > 159) return "\u2586";
-  if (val > 127) return "\u2585";
-  if (val > 95)  return "\u2584";
-  if (val > 63)  return "\u2583";
-  if (val > 31)  return "\u2582";
-  return "_";
-} 
-
-void printColor(uint8_t r, uint8_t g, uint8_t b) {
-  printf("\r%c[%dm", 0x1B, 31); cout << val2block(r);
-  printf("%c[%dm", 0x1B, 32); cout << val2block(g);
-  printf("%c[%dm", 0x1B, 34); cout << val2block(b);
-  printf("%c[%dm", 0x1B, 0);
-  fflush(stdout);
-  usleep(20000);
+struct node
+{
+	int data;
+	node *next;	
+};
+class list
+{
+		private:
+		node *head, *tail;
+		public:
+		list()
+		{
+			head=NULL;
+			tail=NULL;
+		}
+		void createnode(int value)
+		{
+			node *temp=new node;
+			temp->data=value;
+			temp->next=NULL;
+			if(head==NULL)
+			{
+				head=temp;
+				tail=temp;
+				temp=NULL;
+			}
+			else
+			{	
+				tail->next=temp;
+				tail=temp;
+			}
+		}
+		void display()
+		{
+			node *temp=new node;
+			temp=head;
+			while(temp!=NULL)
+			{
+				cout<<temp->data<<"\t";
+				temp=temp->next;
+			}
+            cout<<endl;
+		}
+		void insert_start(int value)
+		{
+			node *temp=new node;
+			temp->data=value;
+			temp->next=head;
+			head=temp;
+		}
+		void insert_position(int pos, int value)
+		{
+			node *pre=new node;
+			node *cur=new node;
+			node *temp=new node;
+			cur=head;
+			for(int i=1;i<pos;i++)
+			{
+				pre=cur;
+				cur=cur->next;
+			}
+			temp->data=value;
+			pre->next=temp;	
+			temp->next=cur;
+		}
+		void delete_first()
+		{
+			node *temp=new node;
+			temp=head;
+			head=head->next;
+			delete temp;
+		}
+		void delete_last()
+		{
+			node *current=new node;
+			node *previous=new node;
+			current=head;
+			while(current->next!=NULL)
+			{
+				previous=current;
+				current=current->next;	
+			}
+			tail=previous;
+			previous->next=NULL;
+			delete current;
+		}
+		void delete_position(int pos)
+		{
+			node *current=new node;
+			node *previous=new node;
+			current=head;
+			for(int i=1;i<pos;i++)
+			{
+				previous=current;
+				current=current->next;
+			}
+			previous->next=current->next;
+		}
+};
+int main()
+{
+	list obj;
+	obj.createnode(25);
+	obj.createnode(50);
+	obj.createnode(90);
+	obj.createnode(40);
+	cout<<"---------------Displaying All nodes---------------\n";
+	obj.display();
+	cout<<"-----------------Inserting At End-----------------\n";
+	obj.createnode(55);
+	obj.display();
+	cout<<"----------------Inserting At Start----------------\n";
+	obj.insert_start(50);
+	obj.display();
+	cout<<"-------------Inserting At Particular--------------\n";
+	obj.insert_position(5,60);
+	obj.display();
+	cout<<"----------------Deleting At Start-----------------\n";
+	obj.delete_first();
+	obj.display();
+	cout<<"-----------------Deleing At End-------------------\n";
+	obj.delete_last();
+	obj.display();
+	cout<<"--------------Deleting At Particular--------------\n";
+	obj.delete_position(4);
+	obj.display();
+	return 0;
 }
-
-#define sendVisualizerPixels(DIST)\
-  r = pixelA[0] << 8;\
-  g = pixelA[1] << 8;\
-  b = pixelA[2] << 8;\
-  printf("%d,%d,%d -> %d,%d,%d over %d steps:\n",r>>8,g>>8,b>>8,pixelB[0],pixelB[1],pixelB[2],SECTION);\
-  r_step = (((uint16_t)pixelB[0] << 8) - ((uint16_t)pixelA[0] << 8))/DIST;\
-  g_step = (((uint16_t)pixelB[1] << 8) - ((uint16_t)pixelA[1] << 8))/DIST;\
-  b_step = (((uint16_t)pixelB[2] << 8) - ((uint16_t)pixelA[2] << 8))/DIST;\
- for (uint8_t p = 0; p < DIST; p++) {\
-    printColor(r>>8, g>>8, b>>8);\
-    r += r_step; g += g_step; b += b_step;\
-  }\
-printf("\r");\
-    
-//----------------------------------------------------------------------- 
-
-int main() {
-
-  /////////////////////////////// start computing and broadcasting each pixel on the fly
-  uint16_t r, g, b;
-  int16_t r_step, g_step, b_step;
-  uint8_t *pixelA, *pixelB;
-  
-  sclera[0][1][0] = 255; sclera[0][1][1] = 255; sclera[0][1][2] = 255;
-  sclera[0][0][0] = 0; sclera[0][0][1] = 0; sclera[0][0][2] = 0;
-  sclera[1][0][0] = 0; sclera[1][0][1] = 255; sclera[1][0][2] = 64;
-  sclera[1][1][0] = 64; sclera[1][1][1] = 0; sclera[1][1][2] = 255;
-
-  pixelA = sclera[0][0]; pixelB = sclera[0][1]; sendVisualizerPixels(SECTION)
-  pixelA = sclera[0][1]; pixelB = sclera[0][0]; sendVisualizerPixels(SECTION)
-  pixelA = sclera[0][0]; pixelB = sclera[1][0]; sendVisualizerPixels(SECTION)
-  pixelA = sclera[1][0]; pixelB = sclera[1][1]; sendVisualizerPixels(SECTION)
-  pixelA = sclera[1][1]; pixelB = sclera[1][0]; sendVisualizerPixels(SECTION)
-  
-}
-
-
