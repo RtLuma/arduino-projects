e79036c2f49da8ef8ed40520400733a7459426eb
Michael Bartlett <bartlettmic@gmail.com>
2018-09-24 23:37:29 -0600

    modules

--- RoomLEDs/prototypes/fadient/linked_loop.hpp
+++ RoomLEDs/prototypes/fadient/linked_loop.hpp
@@ -1,27 +1,21 @@
 #include <iostream>
 #include <string>
-#include <sys/ioctl.h>
-#include <unistd.h>		//usleep
-#include <stdlib.h>		//srand, rand
-#include <time.h>       //time 
-#include <cxxopts.hpp>
 
-
-using namespace std;
+#define ZERO_SYMBOL " "
 
 uint16_t PIXELS;
 uint8_t R, G, B;
 
-#define ZERO_SYMBOL " "
-#define TEST_FAIL_MSG "\n\n\n\n\n\nWeak!\n\n\n\n\n"
-#define SCRAMBLE 20
-// string val2block(uint8_t val) { return "\u2588"; } 
-string val2block(uint8_t val) {if (val > 223) return "\u2588";  if (val > 191) return "\u2587";  if (val > 159) return "\u2586";  if (val > 127) return "\u2585";  if (val > 95)  return "\u2584";  if (val > 63)  return "\u2583";  if (val > 31)  return "\u2582";  return " ";} 
+using voidF = void(*)(int8_t) ;
+
+
+std::string val2block(uint8_t val) { return "\u2588"; } 
+// std::string val2block(uint8_t val) {if (val > 223) return "\u2588";if (val > 191) return "\u2587";if (val > 159) return "\u2586";if (val > 127) return "\u2585";if (val > 95)  return "\u2584";if (val > 63)  return "\u2583";if (val > 31)  return "\u2582";return ZERO_SYMBOL;} 
 
 void printSparkle(int8_t mag) {
 	uint8_t disp = abs(mag); if (disp < 128) disp <<= 1; else disp = 255;
 	printf("\033[38;2;%d;%d;%dm", (uint16_t(R * disp) + 1) >> 8, (uint16_t(G * disp) + 1) >> 8, (uint16_t(B * disp) + 1) >> 8);
-	cout << val2block(disp);
+	std::cout << val2block(disp);
 	printf("\033[0;m");
 	
 } //]]
@@ -29,8 +23,7 @@ void printSparkle(int8_t mag) {
 void printSpawn(int8_t mag) {
 	uint8_t disp = abs(mag); if (disp < 128) disp <<= 1; else disp = 255;
     printf("\033[38;2;255;255;255m");
-	cout << val2block(disp);
-	// cout << "!";
+	std::cout << val2block(disp);
 	printf("\033[0;m");
 	
 } //]]
@@ -38,21 +31,9 @@ void printSpawn(int8_t mag) {
 struct node {
     int8_t mag; uint16_t pos; node *next;
     node(int8_t mag, uint16_t pos, node *next) { this->mag=mag; this->pos=pos; this->next=next; }
-    node() { this->mag=0; this->pos=rand()%SCRAMBLE; this->next=this; }
+    node() { this->mag=0; this->pos=rand()%PIXELS; this->next=this; }
+    voidF nodePrint;
 };
-
-uint8_t printGrad(node* A, node* B) {
-  uint16_t mag = abs(A->mag) << 8; uint16_t dist = B->pos - A->pos;
-  int16_t mag_step = int16_t(abs(B->mag<<8) - mag)/dist;
-  uint16_t p = 1;
-//   printSpawn(mag>>8);
-  printSparkle(mag>>8);
-  mag += mag_step;
-  for (; p < dist; p++) { printSparkle(mag>>8); mag += mag_step; }
-  return dist;
-}
-
-#define EPS 1
 
 int8_t interMag(node* A, node* B, uint16_t pos) {
     uint16_t mag = abs(A->mag) << 8;
@@ -68,9 +49,13 @@ int8_t interMag(node* A, node* B, uint16_t pos) {
 class list {
 private:
     node *head; node *tail; uint8_t nodes;
-
+    voidF nodePrint;
+    
 public:
-    list() { head = new node; tail = head; nodes = 0; }
+    list() { head = new node; tail = head; nodes = 0;
+        nodePrint = printSparkle;
+        // nodePrint = printSpawn;
+    }
     auto getHead()  { return head;  }
     auto getNodes() { return nodes; }
     void populate (uint8_t sparkles) { while (nodes < sparkles) insert(rand()%PIXELS, rand()%256); }
@@ -85,6 +70,18 @@ public:
 		}
 		for (; p < PIXELS; p++) printf(ZERO_SYMBOL);
 	}
+    
+    uint8_t printGrad(node* A, node* B) {
+        uint16_t mag = abs(A->mag) << 8; uint16_t dist = B->pos - A->pos;
+        int16_t mag_step = int16_t(abs(B->mag<<8) - mag)/dist;
+        uint16_t p = 1;
+        //   printSparkle(mag>>8);
+        //   printSpawn(mag>>8);
+        nodePrint(mag>>8);
+        mag += mag_step;
+        for (; p < dist; p++) { printSparkle(mag>>8); mag += mag_step; }
+        return dist;
+    }
     
     void printProx() { node *cur = head; do { printf("[%d,%d] ", cur->pos, cur->mag); cur=cur->next; } while (cur != head); printf("\n"); }
     
@@ -145,13 +142,13 @@ public:
         return false;
     }
     
-    void update() {             // Kill on < midpoint
+    void update() {
       node *pre = tail;
       node *cur = head;
       do {
         int8_t newmag = cur->mag + 1;
         
-        if (abs(abs(newmag) - abs(interMag(pre, cur->next, cur->pos))) < EPS ) { // Regular insert on regular death?
+        if (!(abs(newmag) - abs(interMag(pre, cur->next, cur->pos)))) {
           uint16_t del_pos = cur->pos; cur = cur->next; cut(del_pos);
           bool reinsert; do { del_pos = rand()%PIXELS; reinsert = !insertAlive(del_pos); } while (reinsert);
           continue;
@@ -162,39 +159,3 @@ public:
     }   
     
 };
-
-struct winsize w;
-
-#define SPARKLES 25
-
-int main() {
-    srand(time(NULL));
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-	list sparkles;
-	PIXELS = w.ws_col-2;
-	sparkles.populate(SPARKLES);
-    sparkles.insert(PIXELS,1);
-    R=rand(); G=rand(); B=rand();
-    // R=255; G=64; B=0;
-	while (true) {
-        sparkles.update();
-        cout << "\r[";
-        sparkles.print();
-        cout << "]";
-        fflush(stdout);
-        usleep(20000);
-        printf("\r");
-	}
-    
-    // sparkles.insert(rand()%PIXELS, rand());
-    // sparkles.insert(rand()%PIXELS, rand());
-    // sparkles.insert(rand()%PIXELS, rand());
-    // cout << "\r["; sparkles.print(); cout << "]\n";
-    // sparkles.insertAlive(rand()%PIXELS);
-    // sparkles.insertAlive(rand()%PIXELS);
-    // sparkles.insertAlive(rand()%PIXELS);
-    // sparkles.insertAlive(rand()%PIXELS);
-    // sparkles.insertAlive(rand()%PIXELS);
-    // cout << "\r["; sparkles.print(); cout << "]\n";
-    
-}
