45f46b34916548377ce4376e9643a5672eadac3e
Michael Bartlett <michael.bartlett@nrel.gov>
2018-10-10 23:58:23 -0600

    shit converges yo

--- RoomLEDs/prototypes/fadient/converge.cpp
+++ RoomLEDs/prototypes/fadient/converge.cpp
@@ -17,9 +17,9 @@ void converge(uint8_t rgb[3], uint8_t RGB[3]) {
     for (uint8_t i=0; i<3; i++) {
             int16_t delta = RGB[i]-rgb[i];
             if (!delta) continue;
-            bool delta2 = 128 & delta;
+            bool delta2 = (1<<16) & delta;
             delta /= CONVERGE;
-            if (!delta) delta = delta2 > 0 ? 1 : -1;
+            if (!delta) delta = delta2 ? -1 : 1;
             rgb[i] += delta;
     }
 }
@@ -28,9 +28,9 @@ void converge(uint8_t rgb[3], uint8_t RGB[3]) {
 int main() {
     uint32_t frames=0;
     srand(time(NULL));
-    // for (uint32_t T=0; T<1000; T++) {
+    for (uint32_t T=0; T<1000; T++) {
         for (uint8_t i=0; i<3; i++) { rgb[i]=rand(); RGB[i]=rand(); _RGB[i]=rand(); RGB_[i]=rand();}
-        while(!comp(rgb,RGB)) {
+        while((!comp(rgb,RGB)) && (!comp(RGB,_RGB))) {
             converge(rgb, RGB);
             converge(RGB, _RGB);
             for (uint8_t i=0; i<3; i++) {
@@ -42,5 +42,5 @@ int main() {
             printf("\t\033[38;2;%d;%d;%dm█\033[0;m", _RGB[0], _RGB[1], _RGB[2]);
             printf("\n");
         }
-    // }
+    }
 }
