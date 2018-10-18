8013bbec9623dcdce9f506cab0b8d0806f8850fc
Michael Bartlett <michael.bartlett@nrel.gov>
2018-10-18 17:18:24 -0600

    wew

--- RoomLEDs/prototypes/fadient/fadient.cpp
+++ RoomLEDs/prototypes/fadient/fadient.cpp
@@ -10,7 +10,7 @@ using voidF = void(*)(void) ;
 
 struct winsize w;
 
-uint16_t SPARKLES = 10;
+uint16_t SPARKLES = 20;
 uint16_t PIXELS;
 uint8_t R, G, B;
 voidF display;
@@ -18,11 +18,13 @@ voidF display;
 #define DISCRETE false
 #define TRICHROMATIC true
 
+//https://www.gnu.org/software/libc/manual/html_node/Example-of-Getopt.html
+
 int main() {
     srand(time(NULL));
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
     PIXELS = w.ws_col-2;
-    SPARKLES = PIXELS/20;
+    SPARKLES = PIXELS/10;
     srand(time(NULL));
     R = rand(); G = rand(); B = rand();
     
