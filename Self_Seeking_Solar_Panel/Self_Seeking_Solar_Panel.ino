4408dc60623a75b495ec7ca65ebb5a521499e960
Michael Bartlett <bartlettmic@gmail.com>
2018-11-05 22:26:34 -0700

    time to test osx

---
+++ Self_Seeking_Solar_Panel/Self_Seeking_Solar_Panel.ino
@@ -0,0 +1,27 @@
+#include <avr/interrupt.h> 
+#include <avr/io.h> 
+
+volatile bool ZERO = false;
+
+void YAW_AXIS_ZERO(void)
+{ 
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    digitalWrite(9, !digitalRead(9));
+    digitalWrite(7, !digitalRead(7));
+    ZERO = !ZERO;
+}
+                              
+void setup(void) 
+{ 
+    pinMode(3, INPUT); 
+    pinMode(7, OUTPUT); 
+    pinMode(9, OUTPUT); 
+    attachInterrupt(digitalPinToInterrupt(3), YAW_AXIS_ZERO, RISING);
+    digitalWrite(9, HIGH);
+    digitalWrite(7, LOW);
+    while(!ZERO){};
+} 
+                               
+void loop(void) { 
+  
+} 
