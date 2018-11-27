8538bca51696ebee6efa99157f46915ef27379dc
Michael Bartlett <bartlettmic@gmail.com>
2018-11-27 08:07:44 -0700

    nice

--- Self_Seeking_Solar_Panel/Self_Seeking_Solar_Panel.ino
+++ Self_Seeking_Solar_Panel/Self_Seeking_Solar_Panel.ino
@@ -1,15 +1,15 @@
-#include <avr/interrupt.h> 
-#include <avr/io.h> 
+#include <avr/interrupt.h>
+#include <avr/io.h>
 
-#define YAW_MOTOR_ENABLE 2 // 5
-#define YAW_MOTOR_PIN_1  3 
+#define YAW_MOTOR_PIN_1  9
 #define YAW_MOTOR_PIN_2  4
+#define YAW_MOTOR_ENABLE 5
 
-#define YAW_INTERUPT_PIN 8 // 2
+#define YAW_INTERUPT_PIN 2
 
-#define PITCH_MOTOR_ENABLE 5 // 8
-#define PITCH_MOTOR_PIN_1  6  
-#define PITCH_MOTOR_PIN_2  7  
+#define PITCH_MOTOR_PIN_1  6
+#define PITCH_MOTOR_PIN_2  7
+#define PITCH_MOTOR_ENABLE 8
 
 #define PHOTO_BR A4
 #define PHOTO_TR A7
@@ -18,84 +18,131 @@
 
 volatile bool ZERO = false;
 
-void YAW_AXIS_ZERO(void)
-{ 
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-    digitalWrite(YAW_MOTOR_PIN_1, !digitalRead(YAW_MOTOR_PIN_1));
-    digitalWrite(YAW_MOTOR_PIN_2, !digitalRead(YAW_MOTOR_PIN_2));
-    ZERO = !ZERO;
+void yawTurn(bool ccw) {
+  digitalWrite(YAW_MOTOR_ENABLE, HIGH);
+  digitalWrite(YAW_MOTOR_PIN_2, ccw);
+  digitalWrite(YAW_MOTOR_PIN_1, !ccw);
 }
 
-//void PITCH_AXIS_ZERO(void)
-//{ 
-//    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-//    digitalWrite(3, !digitalRead(3));
-//    digitalWrite(4, !digitalRead(4));
-//    ZERO = !ZERO;
-//}
-
-void setup(void) 
-{ 
-    Serial.begin(9600);
-    pinMode(YAW_MOTOR_ENABLE, OUTPUT); 
-    pinMode(YAW_MOTOR_PIN_1,  OUTPUT); 
-    pinMode(YAW_MOTOR_PIN_2,  OUTPUT); 
-
-    pinMode(PITCH_MOTOR_ENABLE, OUTPUT); 
-    pinMode(PITCH_MOTOR_PIN_1,  OUTPUT); 
-    pinMode(PITCH_MOTOR_PIN_2,  OUTPUT); 
-
-    pinMode(YAW_INTERUPT_PIN,  INPUT); 
-    
-    pinMode(PHOTO_BR, INPUT);
-    pinMode(PHOTO_BL, INPUT);
-    pinMode(PHOTO_TR, INPUT);
-    pinMode(PHOTO_TL, INPUT);
-//    attachInterrupt(digitalPinToInterrupt(YAW_INTERUPT_PIN), YAW_AXIS_ZERO, RISING);
-    
-    digitalWrite(YAW_MOTOR_ENABLE, HIGH);
-    
-    digitalWrite(YAW_MOTOR_PIN_1, HIGH);
-    digitalWrite(YAW_MOTOR_PIN_2, LOW);
-//    while(!ZERO);
-
-} 
-
-uint32_t t = 0;
-                               
-void loop(void) { 
-//  volatile long unsigned int t=0;
-
-//  while (t < 3000000) t++;
-//  
-//  Serial.println(t);
-//  delay(10);
-//  ((void (*)())NULL)();
-
-
-uint32_t right = (analogRead(PHOTO_BR) + analogRead(PHOTO_TR)) >> 1;
-uint32_t left  = (analogRead(PHOTO_BL) + analogRead(PHOTO_TL)) >> 1;
-
-int  delta = right - left;
-
-if ( delta < -1) {
-  digitalWrite(YAW_MOTOR_ENABLE, HIGH);
-  digitalWrite(YAW_MOTOR_PIN_1, LOW);
-  digitalWrite(YAW_MOTOR_PIN_2, HIGH);
-}
-else if (delta > 1) {
-  digitalWrite(YAW_MOTOR_ENABLE, HIGH);
-  digitalWrite(YAW_MOTOR_PIN_1, HIGH);
+void yawStop(void) {
+  digitalWrite(YAW_MOTOR_ENABLE, LOW);
   digitalWrite(YAW_MOTOR_PIN_2, LOW);
+  digitalWrite(YAW_MOTOR_PIN_1, LOW);
 }
-else digitalWrite(YAW_MOTOR_ENABLE, LOW);
 
-//Serial.print(left);
-//Serial.print("\t");
-//Serial.println(right);
+void pitchTurn(bool ccw) {
+  digitalWrite(PITCH_MOTOR_ENABLE, HIGH);
+  digitalWrite(PITCH_MOTOR_PIN_2, ccw);
+  digitalWrite(PITCH_MOTOR_PIN_1, !ccw);
+}
 
-Serial.println(analogRead(PHOTO_BR));
+void pitchStop(void) {
+  digitalWrite(PITCH_MOTOR_ENABLE, LOW);
+  digitalWrite(PITCH_MOTOR_PIN_2, LOW);
+  digitalWrite(PITCH_MOTOR_PIN_1, LOW);
+}
 
-delay(100);
 
-} 
+
+void YAW_AXIS_ZERO(void) {
+  Serial.println("Interupted!");
+  yawStop();
+  for (volatile uint32_t wait = 0; wait < 0xFFFFF;) wait++;
+
+  Serial.println("Interupt ended!");
+  ZERO = !ZERO;
+}
+
+void setup(void) {
+  Serial.begin(9600);
+  digitalWrite(3, LOW); //cuz physics is gay lmao
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+
+  pinMode(YAW_INTERUPT_PIN,  INPUT);
+
+  int OUTPUTS[] = {
+    YAW_MOTOR_ENABLE,
+    YAW_MOTOR_PIN_1,
+    YAW_MOTOR_PIN_2,
+    PITCH_MOTOR_ENABLE,
+    PITCH_MOTOR_PIN_1,
+    PITCH_MOTOR_PIN_2,
+    PHOTO_BR,
+    PHOTO_TR,
+    PHOTO_TL,
+    PHOTO_BL
+  };
+
+  for (auto p : OUTPUTS) pinMode(p, OUTPUT);
+
+  yawTurn(false);
+
+  //  while (!digitalRead(YAW_INTERUPT_PIN)) Serial.println(digitalRead(YAW_INTERUPT_PIN));
+
+  //  attachInterrupt(digitalPinToInterrupt(YAW_INTERUPT_PIN), YAW_AXIS_ZERO, RISING);
+  //  while (!ZERO);
+
+  while (!ZERO) ZERO = digitalRead(YAW_INTERUPT_PIN);
+  digitalWrite(13, LOW);
+  yawTurn(true);
+  delay(4000);
+  digitalWrite(13, HIGH);
+
+  do ZERO = digitalRead(YAW_INTERUPT_PIN); while (!ZERO);
+  digitalWrite(13, LOW);
+  yawTurn(false);
+  delay(1000);
+
+  //
+
+  Serial.println("Initialized");
+
+
+}
+
+
+void loop(void) {
+
+  uint16_t BL = analogRead(PHOTO_BL) + 60,
+           TL = analogRead(PHOTO_TL) + 40,
+           TR = analogRead(PHOTO_TR) + 25,
+           BR = analogRead(PHOTO_BR);
+
+  uint32_t up    = (TR + TL) >> 1;
+  uint32_t down  = (BL + BR) >> 1;
+  uint32_t right = (BR + TR) >> 1;
+  uint32_t left  = (BL + TL) >> 1;
+
+  int32_t dLR = right - left;
+  int32_t dUD = down  - up;
+
+  //  Serial.print(analogRead(PHOTO_BL)+60);
+  //  Serial.print(",");
+  //  Serial.print(analogRead(PHOTO_TL)+40);
+  //  Serial.print(",");
+  //  Serial.print(analogRead(PHOTO_TR)+25);
+  //  Serial.print(",");
+  //  Serial.println(analogRead(PHOTO_BR));
+
+  if (abs(dLR) < 5) yawStop();
+  else yawTurn(dLR < 1);
+
+  if (abs(dUD) < 5) pitchStop();
+  else pitchTurn(dUD < 1);
+
+  //    if ( dUD < -1) {
+  //      digitalWrite(PITCH_MOTOR_ENABLE, HIGH);
+  //      digitalWrite(PITCH_MOTOR_PIN_1, LOW);
+  //      digitalWrite(PITCH_MOTOR_PIN_2, HIGH);
+  //    }
+  //    else if (dUD > 1) {
+  //      digitalWrite(PITCH_MOTOR_ENABLE, HIGH);
+  //      digitalWrite(PITCH_MOTOR_PIN_1, HIGH);
+  //      digitalWrite(PITCH_MOTOR_PIN_2, LOW);
+  //    }
+  //    else digitalWrite(PITCH_MOTOR_ENABLE, LOW);
+
+  delay(100);
+
+}
